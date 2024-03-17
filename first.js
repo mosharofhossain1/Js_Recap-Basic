3ace39cf2d0e5625213da57cae9dc8d3754b27c8
Mosharof <md5470415@gmail.com>
2024-03-17 09:09:37 +0600

    js Basic topic Reacap

---
+++ first.js
@@ -0,0 +1,33 @@
+console.log('Romadan Mubarok');
+
+// Declear  Varibale Category
+
+var x = 23;
+x = 50;
+console.log('This Value is ReAssign :', x)
+
+let a = 34;
+a = 12;
+console.log('This is Value is ReAssign :', a)
+
+const b = 56;
+// b = 78; const variable reassign not supported
+console.log('Const Varible Value Reassign Not Supported', b);
+
+
+
+// Js OPerator 
+
+// 1. Assignment OPerator :- =, +=, -=, /=, *=, **=
+
+let num1 = 34;
+let num2 = 10;
+num2 = 20;
+num2 += 12;
+num2 -= 2;
+num2 /= 5;
+num2 *= 2;
+num2 **= 2;
+console.log(num2)
+
+// 2. Arathmatice OPerator:-   +, -, *, /, %, **
