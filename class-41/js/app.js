7f99a6e083c940faf7bbaa6ea64e10a983133dd3
Mosharof <md5470415@gmail.com>
2024-03-20 12:45:44 +0600

    somthing chnage by js dom

--- class-41/js/app.js
+++ class-41/js/app.js
@@ -36,3 +36,23 @@ console.log(h2);
 
 const pera = document.getElementsByClassName('pera');
 console.log(pera)
+
+// Change Color 
+
+function myFunc(){
+    const h4 = document.getElementById('colorChange');
+    h4.style.color = 'orange';
+    h4.style.fontSize = '30px'
+}
+
+// DIv change element 
+
+function divChange1(){
+    const div1 = document.getElementById('divChange1');
+    div1.style.border = '2px solid red';
+    div1.style.textAlign = 'center';
+    div1.style.marginTop = '20px';
+    div1.style.borderRadius = '10px';
+    div1.style.height = '100px';
+    div1.style.width = '500px'
+}
